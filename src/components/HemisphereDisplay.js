24ab76458737818bfa501e276400ab9c501f5365
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-18 12:07:03 +0100

    new HemisphereConfig component created used for presenting hemisphere representation depend on position

--- src/components/HemisphereDisplay.js
+++ src/components/HemisphereDisplay.js
@@ -1,9 +1,29 @@
 import React from "react";
+import northenPic from "../img/northern-hemisphere.png";
+import southenPic from "../img/southern-hemisphere.png";
+
+const hemisphereConfig = {
+	Northen: {
+		text: "it is northen hemisphere",
+		pic: northenPic,
+	},
+	Southen: {
+		text: "it is southen hemisphere",
+		pic: southenPic,
+	},
+};
 
 const HemisphereDisplay = ({ latitude }) => {
 	// console.log(props.latitude);
-	const hemisphere = latitude > 0 ? "Northen hemisphere" : "Southen hemisphere";
-	return <div>{hemisphere}</div>;
+	const hemisphere = latitude > 0 ? "Northen" : "Southen";
+	const { text, pic } = hemisphereConfig[hemisphere];
+
+	return (
+		<div>
+			<img src={pic} alt='hemisphere representation' />
+			{text}
+		</div>
+	);
 };
 
 export default HemisphereDisplay;
